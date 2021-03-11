9586a4e885efd8c3947034e3823f55d656f1a378
sclaoud <sclaoudFinal7@hotmail.com>
2021-03-11 13:05:20 -0500

    0.11
    Le fait de travailler dans plusieurs fichiers brise les références.  Il est nécessaire de réparer ceux-ci avant de poursuivre.

--- Final/Fonctions.py
+++ Final/Fonctions.py
@@ -5,22 +5,23 @@ Fichier des opérations entre les class et l'interface
 """
 
 # Importation des modules
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton
 
 #   QMessageBox n'était pas importer depuis QtWidgets et doit être forcer ainsi que filedialog
 from PyQt5.QtCore import pyqtProperty, QCoreApplication, QObject, QUrl
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 from classes import *
 from TabGUI import *
 
+ui = Ui_Application
 
 #### Fonctions ####
 class operation:
     ### Enregistrement de l'entré et remise à zero des films ###  à retravailler
     def newFilm(self):
-        film.nomFilm = ui.lineFilm_2.text()
+        film.nomFilm = ui.lineFilm.text()
         film.dureeFilm = ui.timeEdit.time()
         film.descFilm = ui.textDescFilm_2.toPlainText()
 
